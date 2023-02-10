281d76f5b63f80a6390866c4af1b3afaa8aebbb2
Raphael H. M. Medondça <henriquematielli@gmail.com>
2023-02-10 14:15:41 -0300

    Arquivos da página

---
+++ src/Views/footer.php
@@ -0,0 +1,17 @@
+	<footer>
+		<div class="container">
+			<div class="conteudo">
+				<div class="left">
+					Todos direitos reservados.
+				</div>
+				<div class="right">
+					Desenvolvido por Raphael H. M. Mendonça <a href="https://www.instagram.com/raphaelmatielli" target="_blank"><i class="fa-brands fa-instagram"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+    <script src="<?=URL_BASE?>resources/js/jquery/jquery.min.js"></script>
+	<script src="<?=URL_BASE?>resources/js/slick/slick.min.js"></script>
+	<script src="<?=URL_BASE?>resources/js/js.min.js"></script>
+</body>
+</html>
